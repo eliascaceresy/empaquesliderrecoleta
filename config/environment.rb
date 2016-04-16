54d11bd70d94a104ab5067e53fa9d0fcf8adc19f
eliascaceresy <elias.caceres@ceinf.cl>
2016-04-16 15:34:05 -0300

    arreglando mis turnos

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
-module Empaquesrecoleta
-  class Application < Rails::Application
-    config.i18n.default_locale = :es
-  end
-end
